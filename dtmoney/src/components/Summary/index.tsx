64753ada6a92aa7b8a57adffb5b45e4338d65a28
Ericson Scodeler <ericson.br@hotmail.com>
2021-08-14 14:05:56 -0300

    install and configure miragejs

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,31 +1,38 @@
+import { useEffect } from 'react';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
 import { Container } from "./styles";
 
 export function Summary(){
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[])
   return(
     <Container>
       <div>
         <header>
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
-        </header>
-        <strong>R$1.000,00</strong>
+        </header> 
+        <strong>R$ 1.000,00</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong> -R$5.000,00</strong>
+        <strong> -R$ 5.000,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Saídas</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>R$ 1.000,00</strong>
       </div>
     </Container>
   )
